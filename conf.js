4484936d6823aa9a3fdb4e07569b98f4f46261ec
Mihai <mdasmendel@yahoo.com>
2015-10-08 11:46:08 +0300

    add config handler

--- conf.js
+++ conf.js
@@ -18,6 +18,14 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Hello world\n');
 });
+app.get('/config', function (req, res) {
+    request.get('http://45.55.33.155/api/domain-name', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send('Hello world\n');res.send('Hello world\n');
+        }
+    });
+
+});
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
