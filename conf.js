956d6384fd239175f0bc978bd38ad980ca8309f7
Mihai <mdasmendel@yahoo.com>
2015-10-08 11:52:03 +0300

    add config handler

--- conf.js
+++ conf.js
@@ -1,7 +1,7 @@
 /**
  * Created by Mihai on 07.10.2015.
  */
-//var request = require('request');
+var request = require('request');
 //
 //request.get('http://45.55.33.155/api/domain-name', function (error, response, body) {
 //    if (!error && response.statusCode == 200) {
