73d6b0e0511915d0dfaef1cd88168a402f93ddbe
Mihai <mdasmendel@yahoo.com>
2015-10-15 12:42:47 +0300

    .

--- conf.js
+++ conf.js
@@ -81,19 +81,20 @@ var configPostfix = function (hostname) {
 
 
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
-});
+//app.use(function(req, res, next) {
+//    res.header('Access-Control-Allow-Origin', '*');
+//    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//    res.header('Access-Control-Allow-Headers', 'Content-Type');
+//
+//    next();
+//});
 
 app.get('/', function (req, res) {
     res.send('Hello world\n');
 });
 
 app.post('/config-mailer', function (req, res) {
+    console.log(req.body.hostname)
     configPostfix(req.body.hostname)
         .then(connectMilterToPostfix)
         .then(connectPostfixToMilter)
