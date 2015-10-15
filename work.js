e2f87f059c32b55f8b32d26c1a6130cb36f4320c
Mihai <mdasmendel@yahoo.com>
2015-10-15 13:31:36 +0300

    .

--- work.js
+++ work.js
@@ -1,9 +1,7 @@
 var fs = require('fs');
 var Q = require('q');
-var cmd = require('child_process');
 
-var genrateKeyCommand = 'opendkim-genkey -t -s mail -d '
-var copyKeyCommand = 'cp mail.txt  /etc/postfix/dkim.key'
+var dkym = '/etc/postfix/dkim.key'
 
 
 var readFile = function(name){
@@ -18,44 +16,5 @@ var readFile = function(name){
     return deferred.promise
 };
 
-var generateDkym = function(name){
-    var deferred = Q.defer();
-    cmd.exec(
-        genrateKeyCommand +
-        name,
-        function (error, stdout, stderr) {
-            //console.log(1,error);
-            //console.log(2, stdout);
-            //console.log(3, stderr);
-            if (error) {
-                deferred.reject(stderr)
-            } else {
-                deferred.resolve()
 
-            }
-        });
-    return deferred.promise
-};
-
-var copyInPostfix = function(){
-    var deferred = Q.defer();
-    cmd.exec(
-        copyKeyCommand,
-        function (error, stdout, stderr) {
-            if (error) {
-                deferred.reject(stderr)
-            } else {
-                deferred.resolve()
-
-            }
-        });
-    return deferred.promise
-};
-
-generateDkym('mailo.com')
-.then(copyInPostfix)
-.then(function () {
-        console.log('ok');
-    }, function (err) {
-        console.log(err);
-    })
+readFile(dkym);
