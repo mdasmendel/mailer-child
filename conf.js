07d2ea72fc4f810363ba0ff662bc474f7ff938eb
Mihai <mdasmendel@yahoo.com>
2015-10-15 12:35:32 +0300

    work on config handler

--- conf.js
+++ conf.js
@@ -2,42 +2,106 @@
  * Created by Mihai on 07.10.2015.
  */
 var request = require('request');
-
+var express = require('express');
 var bodyParser = require('body-parser');
-//
-//request.get('http://45.55.33.155/api/domain-name', function (error, response, body) {
-//    if (!error && response.statusCode == 200) {
-//        console.log(body) // Show the HTML for the Google homepage.
-//    }
-//});
+var fs = require('fs');
+var Q = require('q');
+
+var app = express();
+
+var socket = 'SOCKET="inet:12301@localhost"\n';
+var posfixMilter = '# DKIM\n' +
+    'milter_protocol = 2\n' +
+    'milter_default_action = accept\n' +
+    'smtpd_milters = inet:localhost:12301\n' +
+    'non_smtpd_milters = inet:localhost:12301\n';
+var postfixConf = '/etc/postfix/main.cf';
+var milterConf = '/etc/default/opendkim';
+
+var connectMilterToPostfix = function(){
+    var deferred = Q.defer();
+    fs.appendFile(milterConf, socket, function (err) {
+        if (err){
+            deferred.reject(err)
+        } else {
+            deferred.resolve()
+        }
+    });
+
+    return deferred.promise;
+
+};
 
 
-var allowCrossDomain = function(req, res, next) {
+var connectPostfixToMilter = function(){
+    var deferred = Q.defer();
+    fs.appendFile(postfixConf, posfixMilter, function (err) {
+        if (err){
+            deferred.reject(err)
+        } else {
+            deferred.resolve()
+        }
+    });
+
+    return deferred.promise;
+
+};
+
+var readFile = function(name){
+    var deferred = Q.defer();
+    fs.readFile(name, 'utf8', function (err, data) {
+        if (err) {
+            deferred.reject(err);
+        } else {
+            deferred.resolve(data)
+        }
+    });
+    return deferred.promise
+};
+
+var configPostfix = function (hostname) {
+    var deferred = Q.defer();
+    readFile(postfixConf)
+        .then(function () {
+            var destination = hostname + ', localhost.localdomain, , localhost';
+            data = data.replace(/myhostname = [^\n]+/gi, 'myhostname = ' + hostname);
+            data = data.replace(/mydestination = [^\n]+/gi, 'mydestination = ' + destination);
+            fs.writeFile(postfixConf, data, function(err) {
+                if(err) {
+                    deferred.reject(err)
+                } else {
+                    deferred.resolve()
+                }
+            });
+        }, function (err) {
+            deferred.reject(err)
+        });
+    return deferred.promise;
+};
+
+
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
     next();
-}
-
-var express = require('express');
-
-// Constants
-var PORT = 8080;
-
-// App
-var app = express();
-
-app.use(bodyParser.json());
-app.use(allowCrossDomain);
+});
 
 app.get('/', function (req, res) {
     res.send('Hello world\n');
 });
 
 app.post('/config-mailer', function (req, res) {
-
-    res.send('Hello world\n');
+    configPostfix(req.body.hostname)
+        .then(connectMilterToPostfix)
+        .then(connectPostfixToMilter)
+        .then(function () {
+            res.send('ok')
+        }, function (err) {
+            res.status(400).send(err)
+        })
 });
 
 app.get('/config', function (req, res) {
@@ -48,6 +112,7 @@ app.get('/config', function (req, res) {
     });
 
 });
+var PORT = 8080;
 
 app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
