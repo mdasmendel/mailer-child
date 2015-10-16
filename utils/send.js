16d88e067894cc42e5c5d6396699b4ec87d32142
Mihai <mdasmendel@yahoo.com>
2015-10-16 11:33:05 +0300

    .

--- utils/send.js
+++ utils/send.js
@@ -30,17 +30,17 @@ var validateDkim = function (domainName) {
     return deferred.promise
 };
 
-//var readLetter = function (domainName) {
-//    var deferred = Q.defer();
-//    fs.readFile(__dirname + '/test.html', 'utf8', function (err, data) {
-//        if (err) {
-//            deferred.reject(err);
-//        } else {
-//            deferred.resolve({letter: data, domainName: domainName})
-//        }
-//    });
-//    return deferred.promise
-//};
+var readLetter = function (domainName) {
+    var deferred = Q.defer();
+    fs.readFile(__dirname + '/test.html', 'utf8', function (err, data) {
+        if (err) {
+            deferred.reject(err);
+        } else {
+            deferred.resolve({letter: data, domainName: domainName})
+        }
+    });
+    return deferred.promise
+};
 
 var sendEmail = function (hostname) {
 
