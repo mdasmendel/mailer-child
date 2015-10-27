7e79807545702130e01fd9981268a9050541fcf8
Mihai <mdasmendel@yahoo.com>
2015-10-27 12:56:57 +0200

    send message

--- utils/mailing_list.js
+++ utils/mailing_list.js
@@ -12,10 +12,25 @@ function addMember(members, list, connn, cb) {
     } else {
         var member = members[0];
         members.splice(0,1);
-        r.branch(
-            r.table(list).getAll(member.email, {index: "email"}).isEmpty(),
+        r.branch(r.table(list).getAll(member.email, {index: "email"}).isEmpty(),
             r.table(list).insert(member),
-            {})
+            {}).run(connn, function (err, result) {
+                if (err) {
+                    cb(err);
+                    member = null;
+                    list = null;
+                    connn = null;
+                } else {
+                    setTimeout(function(){
+                        console.log('remain: ', members.length);
+                        addMember(members,list,connn, cb);
+                        member = null;
+                        list = null;
+                        connn = null;
+                    })
+                }
+
+            })
         ;
     }
 }
