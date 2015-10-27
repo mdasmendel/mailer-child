4c6db308f4dd94535a6239457f10e64ae8b8e329
Mihai <mdasmendel@yahoo.com>
2015-10-27 12:35:16 +0200

    send message

--- utils/mailing_list.js
+++ utils/mailing_list.js
@@ -12,7 +12,7 @@ function addMember(members, list, connn, cb) {
     } else {
         var member = members[0];
         members.splice(0,1);
-        r.branch(r.table(list).getAll(members.email, {index: "email"}).isEmpty(),
+        r.branch(r.table(list).getAll(member.email, {index: "email"}).isEmpty(),
             r.table(list).insert(member, {returnChanges: true}).run(connn, function (err, result) {
                 if (err) {
                     cb(err);
